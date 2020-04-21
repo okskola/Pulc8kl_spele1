af61a33c29a335c167622431466d190d5b91f7f5
okskola <53981730+okskola@users.noreply.github.com>
2020-04-21 15:39:50 +0300

    Pievienoju torņa attēlu

--- tower.py
+++ tower.py
@@ -8,3 +8,6 @@ class Tower( pygame.sprite.Sprite ):
     def __init__(self, game, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.game = game
+        self.image = pygame.image.load(os.path.join(self.game.get_folder_img(),"tower.png")).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
