3a80994db9b827f9692710e083384f50778ce514
snake2205 <mikshenrijs@gmail.com>
2020-04-28 13:28:36 +0300

    Update tower.py
    Pievienoju 3 funkcijas. findDistance() iegūst attālumu, starp mob un torni. reload() , kas atļauj šaut ar noteiktu laika atstarpi. shoot() saliek visu kopā un ja abi nosacījumi ir piepildīti izveido jaunu lodi.

--- tower.py
+++ tower.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import random
+import math
 
 
 class Tower( pygame.sprite.Sprite ):
@@ -11,3 +12,28 @@ class Tower( pygame.sprite.Sprite ):
         self.image = pygame.image.load(os.path.join(self.game.get_folder_img(),"tower.png")).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.topleft = (x,y)
+        self.range = 50#velāk jānomaina skaitlis
+        self.reloadTime = 100# vēlāk jānomaina skaitlis
+
+    def findDistance(mob):
+            (x,y) = mob.rect.center
+            xd = self.rectx - x
+            yd = self.recty - y
+            distance = math.sqrt(xd*xd + yd*yd)
+            return distance
+
+    def reload():
+        if self.reloadTime == 0:
+            return True
+        else:
+            self.reloadTime -= 1
+            
+    def shoot():
+        if reload() == True:
+            for mob in mobs:
+                if findDistance(mob) <= self.range:
+                    self.reloadTime = 100
+                    b = Bullet()# jaieliek parametri lodem
+                    all_sprites.add(b)
+                    bullets.add(b)
+                    break
